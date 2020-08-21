24c2b41ed2b65ed5302b6d5980c43f6832105676
Ian Gregg <iangregg188@gmail.com>
2020-08-21 09:53:48 -0700

    working on models and structure.

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,9 +1,11 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace AnimalShelter.Models
 {
-  public class Animal
-  {
-
-  }
+    public class Animal
+    {
+        public int AnimalId { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
 }
